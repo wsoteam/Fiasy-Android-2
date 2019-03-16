56ea635323f7a3279301ad071b921ac3d6ad630f
Wadim <wk@wsoteam.com>
2019-03-16 12:54:27 +0300

    add pp Activity

---
+++ app/src/main/java/com/wsoteam/diet/OtherActivity/ActivityPrivacyPolicy.java
@@ -0,0 +1,15 @@
+package com.wsoteam.diet.OtherActivity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.wsoteam.diet.R;
+
+public class ActivityPrivacyPolicy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privacy_policy);
+    }
+}
