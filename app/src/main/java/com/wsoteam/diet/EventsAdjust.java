c4baf9a130757fb16a3970a0711b4688b121cda6
Wadim <wk@wsoteam.com>
2019-04-02 14:16:00 +0300

    add events list

---
+++ app/src/main/java/com/wsoteam/diet/EventsAdjust.java
@@ -0,0 +1,49 @@
+package com.wsoteam.diet;
+
+public class EventsAdjust {
+    public static final String first_launch = "first_launch";
+    public static final String session_launch = "session_launch";
+    public static final String first_day = "first_day";
+    public static final String first_week = "first_week";
+    public static final String first_month = "first_month";
+
+    public static final String start_registration = "start_registration";
+    public static final String fill_reg_data = "fill_reg_data";
+    public static final String create_acount = "create_acount";
+    public static final String acept_police = "acept_police";
+    public static final String view_onboarding = "view_onboarding";
+    public static final String view_prem_onboarding = "view_prem_onboarding";
+    public static final String buy_prem_onboarding = "buy_prem_onboarding";
+
+    public static final String attempt_add_food = "attempt_add_food";
+    public static final String view_search_food = "view_search_food";
+    public static final String view_detail_food = "view_detail_food";
+    public static final String success_add_food = "success_add_food";
+    public static final String view_prem_elements = "view_prem_elements";
+    public static final String buy_prem_elements = "buy_prem_elements";
+
+    public static final String view_all_recipes = "view_all_recipes";
+    public static final String view_prem_recipe = "view_prem_recipe";
+    public static final String buy_prem_recipe = "buy_prem_recipe";
+    public static final String view_recipe = "view_recipe";
+
+    public static final String view_all_articles = "view_all_articles";
+    public static final String view_prem_content = "view_prem_content";
+    public static final String buy_prem_content = "buy_prem_content";
+    public static final String view_free_article = "view_free_article";
+    public static final String view_paid_article = "view_paid_article";
+    public static final String share = "share";
+
+    public static final String view_all_trainings = "view_all_trainings";
+    public static final String view_prem_training = "view_prem_training";
+    public static final String buy_prem_training = "buy_prem_training";
+    public static final String view_training = "view_training";
+
+    public static final String add_weight = "add_weight";
+
+    public static final String view_profile = "view_profile";
+    public static final String view_settings = "view_settings";
+    public static final String off_notifications = "off_notifications";
+    public static final String view_prem_profile = "view_prem_profile";
+    public static final String buy_prem_profile = "buy_prem_profile";
+}
