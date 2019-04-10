9a1be86f0001be7d06f76c37935857004e960426
Nikolay <ny@wsoteam.com>
2019-04-11 02:28:10 +0300

    in work

--- app/src/main/java/com/wsoteam/diet/AddMeasurementsActivity.java
+++ app/src/main/java/com/wsoteam/diet/AddMeasurementsActivity.java
@@ -2,12 +2,30 @@ package com.wsoteam.diet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.Button;
 
 public class AddMeasurementsActivity extends AppCompatActivity {
+
+    private Button backButton;
+    private CardView weightCardView;
+    private CardView waistCardView;
+    private CardView chestCardView;
+    private CardView hipsCardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_measurements);
+
+        backButton = findViewById(R.id.btnBackAddMeasur);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 }
