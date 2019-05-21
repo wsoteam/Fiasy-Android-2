deca8a531ba709b28838133747106ec4e07fd7d6
Wadim <wk@wsoteam.com>
2019-05-21 17:10:38 +0300

    fix check null in attr

--- app/src/main/java/com/wsoteam/diet/Amplitude/SetUserProperties.java
+++ app/src/main/java/com/wsoteam/diet/Amplitude/SetUserProperties.java
@@ -8,29 +8,29 @@ public class SetUserProperties {
 
     public static void setUserProperties(AdjustAttribution userProperties) {
         Identify identify = new Identify();
-        if (!userProperties.trackerToken.equals(null)) {
+        if (userProperties.trackerToken != null) {
             identify.set(AtributionsIds.tt, userProperties.trackerToken);
         }
 
-        if (!userProperties.trackerName.equals(null)) {
+        if (userProperties.trackerName != null) {
             identify.set(AtributionsIds.tn, userProperties.trackerName);
         }
-        if (!userProperties.network.equals(null)) {
+        if (userProperties.network != null) {
             identify.set(AtributionsIds.net, userProperties.network);
         }
-        if (!userProperties.campaign.equals(null)) {
+        if (userProperties.campaign != null) {
             identify.set(AtributionsIds.cam, userProperties.campaign);
         }
-        if (!userProperties.adgroup.equals(null)) {
+        if (userProperties.adgroup != null) {
             identify.set(AtributionsIds.adg, userProperties.adgroup);
         }
-        if (!userProperties.creative.equals(null)) {
+        if (userProperties.creative != null) {
             identify.set(AtributionsIds.cre, userProperties.creative);
         }
-        if (!userProperties.clickLabel.equals(null)) {
+        if (userProperties.clickLabel != null) {
             identify.set(AtributionsIds.cl, userProperties.clickLabel);
         }
-        if (!userProperties.adid.equals(null)) {
+        if (userProperties.adid != null) {
             identify.set(AtributionsIds.adid, userProperties.adid);
         }
         Amplitude.getInstance().identify(identify);
