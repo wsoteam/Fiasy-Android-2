5f6fea7f7f71b0b3a2f65a3c03abe966e19d05c9
Paradise9999 <max.karpuk@gmail.com>
2019-06-26 00:56:53 +0300

    Закончена верстка настроек воды

--- app/src/main/java/com/wsoteam/diet/presentation/main/water/WaterActivity.java
+++ app/src/main/java/com/wsoteam/diet/presentation/main/water/WaterActivity.java
@@ -3,7 +3,9 @@ package com.wsoteam.diet.presentation.main.water;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.RadioButton;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.wsoteam.diet.R;
 import com.wsoteam.diet.presentation.global.BaseActivity;
@@ -17,6 +19,9 @@ public class WaterActivity extends BaseActivity {
 
     @BindView(R.id.toolbar) Toolbar toolbar;
     @BindView(R.id.pbWater) SeekBar pbWater;
+    @BindView(R.id.tvWater) TextView tvWater;
+    @BindView(R.id.rbGlass) RadioButton rbGlass;
+    @BindView(R.id.rbBottle) RadioButton rbBottle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,13 +34,43 @@ public class WaterActivity extends BaseActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        pbWater.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tvWater.setText(((float) progress / 10 + 1) + " л");
+                double percent = progress / (double) seekBar.getMax();
+                int offset = seekBar.getThumbOffset();
+                int val = (int) Math.round(percent * (seekBar.getWidth() - 2 * offset));
+                tvWater.setX(offset + seekBar.getX() + val - Math.round(percent * offset) - Math.round(percent * tvWater.getWidth() / 3));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+        pbWater.setMax(30);
+        pbWater.setProgress(1);
+        pbWater.setProgress(0);
     }
 
-    @OnClick({R.id.btnDefault})
+    @OnClick({R.id.btnDefault, R.id.rbGlass, R.id.rbBottle})
     void onClick(View view) {
         switch (view.getId()) {
             case R.id.btnDefault:
+                pbWater.setMax(30);
+                pbWater.setProgress(1);
                 pbWater.setProgress(0);
+                break;
+            case R.id.rbGlass:
+                rbBottle.setChecked(false);
+                break;
+            case R.id.rbBottle:
+                rbGlass.setChecked(false);
                 break;
         }
     }
