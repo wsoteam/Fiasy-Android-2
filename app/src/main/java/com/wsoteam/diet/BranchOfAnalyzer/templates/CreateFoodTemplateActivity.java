18fe863a3660b60a51b979856b6cfc6800789a25
Nikolay <ny@wsoteam.com>
2019-07-02 20:45:34 +0300

    fix

--- app/src/main/java/com/wsoteam/diet/BranchOfAnalyzer/templates/CreateFoodTemplateActivity.java
+++ app/src/main/java/com/wsoteam/diet/BranchOfAnalyzer/templates/CreateFoodTemplateActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import com.wsoteam.diet.R;
 
-public class CreateFoodTempateActivity extends AppCompatActivity {
+public class CreateFoodTemplateActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_food_tempate);
+        setContentView(R.layout.activity_create_food_template);
     }
 }
