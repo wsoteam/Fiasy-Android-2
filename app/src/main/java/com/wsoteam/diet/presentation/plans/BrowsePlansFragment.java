e80427f38c3e5016e54274a25ce30770bce1bd83
Nikolay <ny@wsoteam.com>
2019-07-10 23:24:39 +0300

    init plans branch

---
+++ app/src/main/java/com/wsoteam/diet/presentation/plans/BrowsePlansFragment.java
@@ -0,0 +1,36 @@
+package com.wsoteam.diet.presentation.plans;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.arellomobile.mvp.MvpAppCompatFragment;
+import com.arellomobile.mvp.presenter.InjectPresenter;
+import com.arellomobile.mvp.presenter.ProvidePresenter;
+import com.wsoteam.diet.R;
+
+import butterknife.ButterKnife;
+
+public class BrowsePlansFragment extends MvpAppCompatFragment {
+
+
+    @InjectPresenter
+    BrowsePlansPresenter presenter;
+
+    @ProvidePresenter
+    BrowsePlansPresenter providePresenter() {
+        return new BrowsePlansPresenter();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_browse_plans,
+                container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/wsoteam/diet/presentation/plans/BrowsePlansPresenter.java
@@ -0,0 +1,8 @@
+package com.wsoteam.diet.presentation.plans;
+
+import com.arellomobile.mvp.InjectViewState;
+import com.wsoteam.diet.presentation.global.BasePresenter;
+
+@InjectViewState
+public class BrowsePlansPresenter extends BasePresenter<BrowsePlansView> {
+}
