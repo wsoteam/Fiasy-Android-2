588257727660a23d9e0209ac00557a4b88dcda32
Maksim <max.karpuk@gmail.com>
2019-07-11 14:14:13 +0300

    Правки на главном экране

--- app/src/main/java/com/wsoteam/diet/MainScreen/Fragments/FragmentEatingScroll.java
+++ app/src/main/java/com/wsoteam/diet/MainScreen/Fragments/FragmentEatingScroll.java
@@ -191,20 +191,23 @@ public class FragmentEatingScroll extends Fragment {
         tvCollapsFat.setText(String.valueOf(fat));
         tvCollapsCarbo.setText(String.valueOf(carbo));
 
+        tvCaloriesDone.setText(String.valueOf(kcal));
+        tvCaloriesLeft.setText(String.valueOf(leftKCal));
+
         final String pattern = getString(R.string.main_screen_topbar_string);
 
         if (apCollapsingKcal.getMax() >= kcal) {
             apCollapsingKcal.setProgressDrawable(getResources().getDrawable(R.drawable.progress_bar_calories));
-            tvCaloriesDone.setText(spanText(R.string.main_screen_topbar_kcal_done, String.valueOf(kcal), R.color.main_calories_done));
-            tvCaloriesLeft.setText(spanText(R.string.main_screen_topbar_kcal_left, String.valueOf(leftKCal), R.color.main_calories_left));
+//            tvCaloriesDone.setText(spanText(R.string.main_screen_topbar_kcal_done, String.valueOf(kcal), R.color.main_calories_done));
+//            tvCaloriesLeft.setText(spanText(R.string.main_screen_topbar_kcal_left, String.valueOf(leftKCal), R.color.main_calories_left));
             tvCaloriesNeed.setText(spanText(R.string.main_screen_topbar_left, String.valueOf(apCollapsingKcal.getMax()), R.color.main_calories_done));
             tvCollapsCaloriesNeed.setText(spanText(R.string.main_screen_topbar_left, String.valueOf(apCollapsingKcal.getMax()), R.color.main_calories_done));
             tvCollapsCalories.setTextColor(getResources().getColor(R.color.total_title));
             btnNotification.setVisibility(View.GONE);
         } else {
             apCollapsingKcal.setProgressDrawable(getResources().getDrawable(R.drawable.progress_bar_calories_over));
-            tvCaloriesDone.setText(spanText(R.string.main_screen_topbar_kcal_done, String.valueOf(kcal), R.color.main_calories_done_over));
-            tvCaloriesLeft.setText(spanText(R.string.main_screen_topbar_kcal_over, "+" + Math.abs(leftKCal), R.color.main_calories_left_over));
+//            tvCaloriesDone.setText(spanText(R.string.main_screen_topbar_kcal_done, String.valueOf(kcal), R.color.main_calories_done_over));
+//            tvCaloriesLeft.setText(spanText(R.string.main_screen_topbar_kcal_over, "+" + Math.abs(leftKCal), R.color.main_calories_left_over));
             tvCaloriesNeed.setText(spanText(R.string.main_screen_topbar_left, String.valueOf(apCollapsingKcal.getMax()), R.color.main_calories_done_over));
             tvCollapsCaloriesNeed.setText(spanText(R.string.main_screen_topbar_left, String.valueOf(apCollapsingKcal.getMax()), R.color.main_calories_done_over));
             tvCollapsCalories.setTextColor(getResources().getColor(R.color.main_calories_done_over));
