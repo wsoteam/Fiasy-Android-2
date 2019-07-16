df71c876c0913440c568ea200289308d24c59f27
Wadim <wk@wsoteam.com>
2019-07-16 16:55:03 +0300

    ui act about
    tn-5362

--- app/src/main/java/com/wsoteam/diet/presentation/profile/about/AboutActivity.java
+++ app/src/main/java/com/wsoteam/diet/presentation/profile/about/AboutActivity.java
@@ -1,6 +1,7 @@
 package com.wsoteam.diet.presentation.profile.about;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 
 import com.arellomobile.mvp.MvpAppCompatActivity;
 import com.arellomobile.mvp.presenter.InjectPresenter;
@@ -9,6 +10,8 @@ import com.wsoteam.diet.R;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import dagger.android.AndroidInjection;
 
 public class AboutActivity extends MvpAppCompatActivity implements AboutView {
@@ -30,5 +33,7 @@ public class AboutActivity extends MvpAppCompatActivity implements AboutView {
         AndroidInjection.inject(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about_activity);
+        ButterKnife.bind(this);
+
     }
 }
