7f18ad6b8199e7c3707c6ff9546720da6df2e2bd
Wadim <wk@wsoteam.com>
2019-07-19 12:58:41 +0300

    custom shape marker
    TN-5331
    /
    
    TN-5613

--- app/src/main/java/com/wsoteam/diet/common/views/bar/marker/BarMarker.java
+++ app/src/main/java/com/wsoteam/diet/common/views/bar/marker/BarMarker.java
@@ -1,9 +1,11 @@
 package com.wsoteam.diet.common.views.bar.marker;
 
 import android.content.Context;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.github.mikephil.charting.components.MarkerView;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.CandleEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.highlight.Highlight;
@@ -13,22 +15,32 @@ import com.wsoteam.diet.R;
 
 public class BarMarker extends MarkerView {
     private TextView tvTitle;
+    private int[] colors;
 
-    public BarMarker(Context context, int layoutResource) {
+    public BarMarker(Context context, int layoutResource, int[] colors) {
         super(context, layoutResource);
         tvTitle = findViewById(R.id.tvContent);
+        this.colors = colors;
     }
 
     @Override
     public void refreshContent(Entry e, Highlight highlight) {
+        tvTitle.setTextColor(colors[highlight.getDataSetIndex()]);
+        Log.e("LOL", String.valueOf(highlight.getStackIndex()));
+        Log.e("LOL", String.valueOf(highlight.getDataSetIndex()));
+        Log.e("LOL", String.valueOf(e.getData()));
         if (e instanceof CandleEntry) {
             CandleEntry ce = (CandleEntry) e;
-            tvTitle.setText("" + Utils.formatNumber(ce.getHigh(), 0, true) + getContext().getString(R.string.marker_kcal));
+            BarEntry barEntry = (BarEntry) e;
+            tvTitle.setText("" + Utils.formatNumber(ce.getHigh(), 0, true) + " " + getContext().getString(R.string.marker_kcal));
+
         } else {
-            tvTitle.setText("" + Utils.formatNumber(e.getY(), 0, true) + getContext().getString(R.string.marker_kcal));
+            tvTitle.setText("" + Utils.formatNumber(e.getY(), 0, true) + " " + getContext().getString(R.string.marker_kcal));
         }
         super.refreshContent(e, highlight);
     }
+
+
 
     @Override
     public MPPointF getOffset() {
--- app/src/main/java/com/wsoteam/diet/presentation/profile/section/ProfileFragment.java
+++ app/src/main/java/com/wsoteam/diet/presentation/profile/section/ProfileFragment.java
@@ -118,14 +118,15 @@ public class ProfileFragment extends MvpAppCompatFragment implements ProfileView
         YAxis yAxisLeft = gv.getAxisLeft();
         yAxisLeft.setAxisMinimum(0f);
         gv.setMarker(new MarkerView(getActivity(), R.layout.marker_calories));
-        BarMarker barMarker = new BarMarker(getActivity(), R.layout.marker_calories);
-        gv.setMarker(barMarker);
+        BarMarker barMarker = new BarMarker(getActivity(), R.layout.marker_calories, colors);
+
 
         gv.setDrawBarShadow(false);
         gv.getDescription().setEnabled(false);
         gv.setDrawGridBackground(false);
         gv.getLegend().setEnabled(false);
         gv.setData(barData);
+        gv.setMarker(barMarker);
     }
 
     @Override
