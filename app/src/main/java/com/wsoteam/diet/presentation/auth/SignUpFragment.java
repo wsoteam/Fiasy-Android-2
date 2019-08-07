84669e33074bbd956d1caa75fc3104bfa1ec0ce9
ozz <ozik.dev@gmail.com>
2019-08-07 21:05:36 +0300

    - fixed buttons state in sign in / sign up - added restore password flow
    TN-6829 TN-7035 TN-7036

--- app/src/main/java/com/wsoteam/diet/presentation/auth/SignUpFragment.java
+++ app/src/main/java/com/wsoteam/diet/presentation/auth/SignUpFragment.java
@@ -1,14 +1,20 @@
 package com.wsoteam.diet.presentation.auth;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import com.google.android.material.textfield.TextInputLayout;
 import com.wsoteam.diet.R;
 import com.wsoteam.diet.presentation.auth.EmailLoginAuthStrategy.Account;
+import com.wsoteam.diet.utils.IntentUtils;
+import com.wsoteam.diet.utils.RichTextUtils;
+
+import static android.text.TextUtils.concat;
 
 public class SignUpFragment extends SignInFragment {
 
@@ -16,6 +22,25 @@ public class SignUpFragment extends SignInFragment {
   public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
       @Nullable Bundle savedInstanceState) {
     return inflater.inflate(R.layout.fragment_auth_signup, container, false);
+  }
+
+  @Override public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+
+    final TextView privacyView = view.findViewById(R.id.privacyPolicy);
+
+    if (privacyView != null) {
+      final RichTextUtils.RichText actionOpenPrivacyPolicy =
+          new RichTextUtils.RichText(getString(R.string.sign_up_privacy_policy_link_span_text))
+              .colorRes(requireContext(), R.color.blue)
+              .underline()
+              .onClick(v -> IntentUtils.openWebLink(v.getContext(),
+                  "http://fiasy.com/PrivacyPolice.html"));
+
+      privacyView.setMovementMethod(LinkMovementMethod.getInstance());
+      privacyView.setText(concat(getString(R.string.sign_up_privacy_policy_title), " ",
+          actionOpenPrivacyPolicy.text()));
+    }
   }
 
   @Override protected Account getUserInputAccount() {
