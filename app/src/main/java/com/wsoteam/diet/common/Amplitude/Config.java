02b87cbb76b8cfeb35f9f0eca32ad40610f3dc17
Wadim <waadimka@mail.ru>
2019-08-09 12:59:10 +0300

    names events

---
+++ app/src/main/java/com/wsoteam/diet/common/Amplitude/Config.java
@@ -0,0 +1,65 @@
+package com.wsoteam.diet.common.Amplitude;
+
+public class Config {
+    //ONBOARD
+    public static final String ONBOARING_NEXT = "onboarding_next";
+    public static final String ONBOARING_SKIP = "onboarding_skip";
+    public static final String REGISTRATION_SUCCESS = "registration_success";
+    public static final String REGISTRATION_ERROR = "registration_error";
+    public static final String ENTER_SUCCESS = "enter_success";
+    public static final String ENTER_ERROR = "enter_error";
+    public static final String REGISRTATION_PRIVACY = "registration_privacy";
+    public static final String RESEND_SUCCESS = "resend_success";
+    public static final String RESEND_ERROR = "resend_error";
+    public static final String QUESTION_NEXT = "question_next";
+
+    //PURCHASE
+    public static final String TRIAL_SUCCES = "trial_success";
+    public static final String TRIAL_ERROR = "trial_error";
+    public static final String PURCHASE_SUCCESS = "purchase_success";
+    public static final String PURCHASE_ERROR = "purchase_error";
+    public static final String REVENUE = "revenue";
+
+    //FOOD
+    public static final String ADD_FOOD_SUCCESS = "add_food_success";
+    public static final String EDIT_FOOD = "edit_food";
+    public static final String DELETE_FOOD = "delete_food";
+    public static final String FOOD_SEARCH = "food_search";
+    public static final String VIEW_PRODUCT_PAGE = "view_product_page";
+    public static final String PRODUCT_PAGE_FAVORITES = "product_page_favorites";
+    public static final String PRODUCT_PAGE_SHARE = "product_page_share";
+    public static final String PRODUCT_PAGE_BUGSEND = "product_page_bugsend";
+    public static final String PRODUCT_PAGE_MICRO = "product_page_micro";
+    public static final String CUSTOM_PRODUCT_SUCCESS = "custom_product_success";
+    public static final String CUSTOM_RECIPE_SUCCESS = "custom_recipe_success";
+    public static final String CUSTOM_TEMPLATE_SUCCESS = "custom_template_success";
+
+    //RECIPE
+    public static final String VIEW_RECIPE = "view_recipe";
+    public static final String RECIPE_CATEGORY = "recipe_category";
+    public static final String RECIPE_FAVORITES = "recipe_favorites";
+    public static final String RECIPE_ADD_SUCCES = "recipe_add_success";
+
+    //PROFILE
+    public static final String VIEW_PROFILE = "view_profile";
+    public static final String PROFILE_LOGOUT = "profile_logout";
+
+    //INTERCOM
+    public static final String INTERCOM_CHAT = "intercom_chat";
+
+    //ARTICLES
+    public static final String CHOOSE_ARTICLES = "choose_articles";
+    public static final String VIEW_ARTICLES = "view_articles";
+    public static final String SELECT_DIET = "select_diet";
+    public static final String SELECT_TRAINING = "select_training";
+
+    //DIET
+    public static final String SELECT_DIET_FILTRES = "select_diet_filters";
+    public static final String SHARE_DIET = "share_diet";
+
+    //TRAINING
+    public static final String SELECT_TRAINING = "select_training";
+    public static final String SHARE_TRAINING = "share_training";
+
+
+}
