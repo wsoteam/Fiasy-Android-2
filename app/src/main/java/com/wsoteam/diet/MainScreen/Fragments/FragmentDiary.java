799942ac37e008bd91e6a03f9174ee67841ee765
ozz <ozik.dev@gmail.com>
2019-08-15 19:25:43 +0300

    - fixed merging
    TN-6829
    TN-7694
    TN-7696
    TN-7695

--- app/src/main/java/com/wsoteam/diet/MainScreen/Fragments/FragmentDiary.java
+++ app/src/main/java/com/wsoteam/diet/MainScreen/Fragments/FragmentDiary.java
@@ -5,35 +5,35 @@ import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import com.google.android.material.appbar.AppBarLayout;
-import com.google.android.material.appbar.CollapsingToolbarLayout;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.ViewPager;
-import androidx.appcompat.app.AlertDialog;
-import androidx.cardview.widget.CardView;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 import com.amplitude.api.Amplitude;
 import com.github.jhonnyx2012.horizontalpicker.DatePickerListener;
 import com.github.jhonnyx2012.horizontalpicker.HorizontalPicker;
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.wsoteam.diet.AmplitudaEvents;
-import com.wsoteam.diet.BranchProfile.Fragments.FragmentProfile;
 import com.wsoteam.diet.Config;
 import com.wsoteam.diet.MainScreen.Dialogs.SublimePickerDialogFragment;
 import com.wsoteam.diet.MainScreen.intercom.IntercomFactory;
@@ -41,17 +41,10 @@ import com.wsoteam.diet.POJOProfile.Profile;
 import com.wsoteam.diet.R;
 import com.wsoteam.diet.Sync.UserDataHolder;
 import com.wsoteam.diet.Sync.WorkWithFirebaseDB;
-
+import io.intercom.android.sdk.Intercom;
+import java.util.Calendar;
 import org.joda.time.DateTime;
 import org.joda.time.Days;
-
-import java.util.Calendar;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-import butterknife.Unbinder;
-import io.intercom.android.sdk.Intercom;
 
 import static android.content.Context.MODE_PRIVATE;
 
@@ -91,18 +84,11 @@ public class FragmentDiary extends Fragment implements SublimePickerDialogFragme
     HorizontalPicker datePicker;
     @BindView(R.id.btnNotification)
     ImageView btnNotification;
-    private BottomNavigationView bnvMain;
     private Unbinder unbinder;
-    private Profile profile;
-    private int COUNT_OF_RUN = 0;
-    private FragmentTransaction transaction;
     private int dayPosition = Config.COUNT_PAGE;
     private SharedPreferences countOfRun;
-    private boolean isFiveStarSend = false;
     private AlertDialog alertDialogBuyInfo;
-    private SharedPreferences sharedPreferences, freeUser;
     private LinearLayout.LayoutParams layoutParams;
-    private Window window;
     private ViewPager.OnPageChangeListener viewPagerListener = new ViewPager.OnPageChangeListener() {
         @Override
         public void onPageScrolled(int i, float v, int i1) {
@@ -123,14 +109,15 @@ public class FragmentDiary extends Fragment implements SublimePickerDialogFragme
         }
     };
 
+    public FragmentDiary() {
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View mainView = inflater.inflate(R.layout.activity_main_new, container, false);
         unbinder = ButterKnife.bind(this, mainView);
         getActivity().setTitle("");
-        bnvMain = getActivity().findViewById(R.id.bnv_main);
-        transaction = getActivity().getSupportFragmentManager().beginTransaction();
         Amplitude.getInstance().logEvent(AmplitudaEvents.view_diary);
         /** on your logout method:**/
         Intent broadcastIntent = new Intent();
@@ -167,7 +154,8 @@ public class FragmentDiary extends Fragment implements SublimePickerDialogFragme
             alertDialogBuyInfo.getWindow().setBackgroundDrawable(new ColorDrawable(0));
             alertDialogBuyInfo.show();
 
-            sharedPreferences = getActivity().getSharedPreferences(Config.ALERT_BUY_SUBSCRIPTION, MODE_PRIVATE);
+            SharedPreferences sharedPreferences =
+                getActivity().getSharedPreferences(Config.ALERT_BUY_SUBSCRIPTION, MODE_PRIVATE);
             SharedPreferences.Editor editor = sharedPreferences.edit();
             editor.putBoolean(Config.ALERT_BUY_SUBSCRIPTION, false);
             editor.apply();
@@ -250,7 +238,9 @@ public class FragmentDiary extends Fragment implements SublimePickerDialogFragme
     private void additionOneToSharedPreference() {
         countOfRun = getActivity().getPreferences(MODE_PRIVATE);
         SharedPreferences.Editor editor = countOfRun.edit();
-        editor.putInt(TAG_COUNT_OF_RUN_FOR_ALERT_DIALOG, countOfRun.getInt(TAG_COUNT_OF_RUN_FOR_ALERT_DIALOG, COUNT_OF_RUN) + 1);
+        int COUNT_OF_RUN = 0;
+        editor.putInt(TAG_COUNT_OF_RUN_FOR_ALERT_DIALOG, countOfRun.getInt(TAG_COUNT_OF_RUN_FOR_ALERT_DIALOG,
+            COUNT_OF_RUN) + 1);
         editor.apply();
     }
 
@@ -259,7 +249,7 @@ public class FragmentDiary extends Fragment implements SublimePickerDialogFragme
         return countOfRun.getInt(TAG_COUNT_OF_RUN_FOR_ALERT_DIALOG, 0);
     }
 
-    @OnClick({R.id.fabAddEating, R.id.btnNotification, R.id.tvReports})
+    @OnClick({R.id.fabAddEating, R.id.btnNotification})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.fabAddEating:
@@ -269,13 +259,6 @@ public class FragmentDiary extends Fragment implements SublimePickerDialogFragme
                 break;
             case R.id.btnNotification:
                 attachCaloriesPopup();
-                break;
-            case R.id.tvReports:
-                window = getActivity().getWindow();
-                window.setStatusBarColor(Color.parseColor("#2E4E4E"));
-                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-                transaction.replace(R.id.flFragmentContainer, new FragmentProfile()).commit();
-                bnvMain.setSelectedItemId(R.id.bnv_main_profile);
                 break;
         }
     }
@@ -297,7 +280,7 @@ public class FragmentDiary extends Fragment implements SublimePickerDialogFragme
         super.onResume();
         IntercomFactory.show();
         if (UserDataHolder.getUserData() != null && UserDataHolder.getUserData().getProfile() != null) {
-            profile = UserDataHolder.getUserData().getProfile();
+            Profile profile = UserDataHolder.getUserData().getProfile();
             setMaxParamsInProgressBars(profile);
             tvDaysAtRow.setText(getActivity().getResources().getQuantityString(R.plurals.daysAtRow, getDaysAtRow(), getDaysAtRow()));
         }
--- app/src/main/java/com/wsoteam/diet/MainScreen/Fragments/FragmentEatingScroll.java
+++ app/src/main/java/com/wsoteam/diet/MainScreen/Fragments/FragmentEatingScroll.java
@@ -5,6 +5,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
