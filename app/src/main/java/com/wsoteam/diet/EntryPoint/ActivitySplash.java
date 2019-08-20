4152de9d3127f1d88e3b0537531928241cab5a68
ozz <ozik.dev@gmail.com>
2019-08-20 14:51:29 +0300

    - fixed git merge issues
    TN-6829
    TN-7972

--- app/src/main/java/com/wsoteam/diet/EntryPoint/ActivitySplash.java
+++ app/src/main/java/com/wsoteam/diet/EntryPoint/ActivitySplash.java
@@ -28,6 +28,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseException;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
@@ -123,9 +124,15 @@ public class ActivitySplash extends BaseActivity {
       myRef.addListenerForSingleValueEvent(new ValueEventListener() {
         @Override
         public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-          new UserDataHolder().bindObjectWithHolder(dataSnapshot.getValue(UserData.class));
-          setUserProperties(UserDataHolder.getUserData().getProfile());
-          onSignedIn();
+          try {
+            new UserDataHolder().bindObjectWithHolder(dataSnapshot.getValue(UserData.class));
+            setUserProperties(UserDataHolder.getUserData().getProfile());
+            onSignedIn();
+          } catch (DatabaseException e){
+            e.printStackTrace();
+
+            WorkWithFirebaseDB.dropUserMealsDiary();
+          }
         }
 
         @Override
