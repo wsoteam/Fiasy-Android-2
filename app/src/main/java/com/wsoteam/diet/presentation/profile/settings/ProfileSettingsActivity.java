177e5353219356db475f2dc4bf6980d735eb189e
Wadim <waadimka@mail.ru>
2019-08-28 18:25:09 +0300

    ui head
    TN-8359
    /
    
    TN-8455

--- app/src/main/java/com/wsoteam/diet/presentation/profile/settings/ProfileSettingsActivity.java
+++ app/src/main/java/com/wsoteam/diet/presentation/profile/settings/ProfileSettingsActivity.java
@@ -1,21 +1,26 @@
 package com.wsoteam.diet.presentation.profile.settings;
 
 import android.os.Bundle;
+
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
+
 import com.arellomobile.mvp.MvpAppCompatActivity;
 import com.arellomobile.mvp.presenter.ProvidePresenter;
 import com.wsoteam.diet.Config;
 import com.wsoteam.diet.R;
 import com.wsoteam.diet.presentation.profile.settings.controller.ItemsAdapter;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class ProfileSettingsActivity extends MvpAppCompatActivity implements ProfileSettingsView {
     ProfileSettingsPresenter profileSettingsPresenter;
     @BindView(R.id.rvSettingsItems) RecyclerView rvSettingsItems;
+    @BindView(R.id.cvPremium) CardView cvPremium;
 
 
     @ProvidePresenter
@@ -34,7 +39,8 @@ public class ProfileSettingsActivity extends MvpAppCompatActivity implements Pro
 
         rvSettingsItems.setLayoutManager(new LinearLayoutManager(this));
         rvSettingsItems.setAdapter(new ItemsAdapter(this, !getSharedPreferences(Config.STATE_BILLING, MODE_PRIVATE)
-            .getBoolean(Config.STATE_BILLING, false)));
+                .getBoolean(Config.STATE_BILLING, false)));
+        cvPremium.setBackgroundResource(R.drawable.shape_prem_settings);
     }
 
     @OnClick(R.id.ibBack)
