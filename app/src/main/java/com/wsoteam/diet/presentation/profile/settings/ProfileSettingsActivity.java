c64d942f0e30dd3c724afd813a71fd196b53d2ca
Wadim <waadimka@mail.ru>
2019-08-28 18:55:59 +0300

    mech head
    TN-8359
    /
    
    TN-8487

--- app/src/main/java/com/wsoteam/diet/presentation/profile/settings/ProfileSettingsActivity.java
+++ app/src/main/java/com/wsoteam/diet/presentation/profile/settings/ProfileSettingsActivity.java
@@ -1,6 +1,8 @@
 package com.wsoteam.diet.presentation.profile.settings;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
@@ -9,8 +11,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.arellomobile.mvp.MvpAppCompatActivity;
 import com.arellomobile.mvp.presenter.ProvidePresenter;
+import com.wsoteam.diet.AmplitudaEvents;
+import com.wsoteam.diet.Authenticate.POJO.Box;
 import com.wsoteam.diet.Config;
+import com.wsoteam.diet.InApp.ActivitySubscription;
 import com.wsoteam.diet.R;
+import com.wsoteam.diet.common.Analytics.EventProperties;
+import com.wsoteam.diet.common.settings.PremiumCloseStateSingleton;
 import com.wsoteam.diet.presentation.profile.settings.controller.ItemsAdapter;
 
 import butterknife.BindView;
@@ -41,10 +48,34 @@ public class ProfileSettingsActivity extends MvpAppCompatActivity implements Pro
         rvSettingsItems.setAdapter(new ItemsAdapter(this, !getSharedPreferences(Config.STATE_BILLING, MODE_PRIVATE)
                 .getBoolean(Config.STATE_BILLING, false)));
         cvPremium.setBackgroundResource(R.drawable.shape_prem_settings);
+
+        if (getSharedPreferences(Config.STATE_BILLING, MODE_PRIVATE)
+                .getBoolean(Config.STATE_BILLING, false) || PremiumCloseStateSingleton.getInstance().isClosePremium()) {
+            cvPremium.setVisibility(View.GONE);
+        }
     }
 
-    @OnClick(R.id.ibBack)
-    public void onViewClicked() {
-        onBackPressed();
+
+    @OnClick({R.id.btnSettingsPrem, R.id.ibSettingsPremClose, R.id.ibBack})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btnSettingsPrem:
+                Intent intent = new Intent(this, ActivitySubscription.class);
+                Box box = new Box();
+                box.setComeFrom(AmplitudaEvents.view_prem_settings);
+                box.setBuyFrom(EventProperties.trial_from_settings);
+                box.setOpenFromPremPart(true);
+                box.setOpenFromIntrodaction(false);
+                intent.putExtra(Config.TAG_BOX, box);
+                startActivity(intent);
+                break;
+            case R.id.ibSettingsPremClose:
+                cvPremium.setVisibility(View.GONE);
+                PremiumCloseStateSingleton.getInstance().setClosePremium(true);
+                break;
+            case R.id.ibBack:
+                onBackPressed();
+                break;
+        }
     }
 }
