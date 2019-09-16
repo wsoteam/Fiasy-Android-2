d72a0027426bf9be1e19808767ac05b7c1660494
Wadim <waadimka@mail.ru>
2019-09-16 16:31:23 +0300

    attempt fix null userData
    TN-8380
    /
    
    TN-9937
    copy current issue url

--- app/src/main/java/com/wsoteam/diet/Sync/UserDataHolder.java
+++ app/src/main/java/com/wsoteam/diet/Sync/UserDataHolder.java
@@ -1,5 +1,14 @@
 package com.wsoteam.diet.Sync;
 
+import androidx.annotation.NonNull;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.wsoteam.diet.Config;
 import com.wsoteam.diet.Sync.POJO.UserData;
 
 public class UserDataHolder {
@@ -10,10 +19,32 @@ public class UserDataHolder {
     }
 
     public static UserData getUserData() {
-        return userData;
+        if (userData != null) {
+            return userData;
+        }else {
+
+        }
     }
 
     public static void clearObject() {
         userData = new UserData();
+    }
+
+    private static void reloadUserData(){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference(Config.NAME_OF_USER_DATA_LIST_ENTITY).
+                child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                userData = dataSnapshot.getValue(UserData.class);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
