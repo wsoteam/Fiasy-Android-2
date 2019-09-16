c2f99f09f571b95c5b330af04cbf4d6c5adadef6
Wadim <waadimka@mail.ru>
2019-09-16 16:38:48 +0300

    delete attempt

--- app/src/main/java/com/wsoteam/diet/Sync/UserDataHolder.java
+++ app/src/main/java/com/wsoteam/diet/Sync/UserDataHolder.java
@@ -1,14 +1,5 @@
 package com.wsoteam.diet.Sync;
 
-import androidx.annotation.NonNull;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.wsoteam.diet.Config;
 import com.wsoteam.diet.Sync.POJO.UserData;
 
 public class UserDataHolder {
@@ -19,32 +10,10 @@ public class UserDataHolder {
     }
 
     public static UserData getUserData() {
-        if (userData != null) {
-            return userData;
-        }else {
-
-        }
+        return userData;
     }
 
     public static void clearObject() {
         userData = new UserData();
-    }
-
-    private static void reloadUserData(){
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference(Config.NAME_OF_USER_DATA_LIST_ENTITY).
-                child(FirebaseAuth.getInstance().getCurrentUser().getUid());
-
-        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                userData = dataSnapshot.getValue(UserData.class);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
     }
 }
