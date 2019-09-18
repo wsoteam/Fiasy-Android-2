de6fc241ac8aedc23d8a1dd3c21e6e3b6031d28b
Wadim <waadimka@mail.ru>
2019-09-18 13:03:55 +0300

    interactive ui
    TN-10034
    /
    
    TN-10174
    copy current issue url

--- app/src/main/java/com/wsoteam/diet/presentation/promo/PromoFormActivity.java
+++ app/src/main/java/com/wsoteam/diet/presentation/promo/PromoFormActivity.java
@@ -12,12 +12,14 @@ import com.wsoteam.diet.R;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import butterknife.OnTextChanged;
 
 public class PromoFormActivity extends MvpAppCompatActivity implements PromoFormView {
     @BindView(R.id.btnSendPromo) Button btnSendPromo;
     @BindView(R.id.edtPromo) EditText edtPromo;
     @BindView(R.id.tilPromo) TextInputLayout tilPromo;
     private PromoFormPresenter presenter;
+    private boolean isError = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +29,58 @@ public class PromoFormActivity extends MvpAppCompatActivity implements PromoForm
 
         presenter = new PromoFormPresenter(this);
         presenter.attachView(this);
+        tilPromo.setError("Введите промокод");
+        tilPromo.setErrorTextColor(getResources().getColorStateList(R.color.hint_promo));
+    }
+
+    @OnTextChanged(value = R.id.edtPromo, callback = OnTextChanged.Callback.TEXT_CHANGED)
+    public void promoChanged(CharSequence text) {
+        if (text.length() > 0){
+            onSendMode();
+        }else {
+            offSendMode();
+        }
+        if (isError){
+            hideError();
+        }
+    }
+
+    private void onSendMode(){
+        if (!btnSendPromo.isEnabled()) {
+            btnSendPromo.setEnabled(true);
+            btnSendPromo.setBackgroundTintList(getResources().getColorStateList(R.color.active_promo_btn));
+        }
+    }
+
+    private void offSendMode(){
+        if (btnSendPromo.isEnabled()) {
+            btnSendPromo.setEnabled(false);
+            btnSendPromo.setBackgroundTintList(getResources().getColorStateList(R.color.unactive_promo_btn));
+        }
+    }
+
+    private void showError(){
+        isError = true;
+        edtPromo.setBackgroundColor(getResources().getColor(R.color.hint_error));
+        tilPromo.setError("Промокод введен не верно");
+        tilPromo.setErrorTextColor(getResources().getColorStateList(R.color.hint_error));
+    }
+
+    private void hideError(){
+        isError = false;
+        edtPromo.setBackgroundColor(getResources().getColor(R.color.hint_no_error_back));
+        tilPromo.setError("Введите промокод");
+        tilPromo.setErrorTextColor(getResources().getColorStateList(R.color.hint_promo));
     }
 
     @OnClick({R.id.ibBack, R.id.btnSendPromo})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.ibBack:
+                onBackPressed();
                 break;
             case R.id.btnSendPromo:
+                showError();
                 break;
         }
     }
