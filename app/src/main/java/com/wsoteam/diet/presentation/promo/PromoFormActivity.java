e6ea2f7733ba4ca6514f658b83f5cfbc0cf01f50
Wadim <waadimka@mail.ru>
2019-09-18 12:36:49 +0300

    ui fin
    TN-10034
    /
    
    TN-10172
    copy current issue url

--- app/src/main/java/com/wsoteam/diet/presentation/promo/PromoFormActivity.java
+++ app/src/main/java/com/wsoteam/diet/presentation/promo/PromoFormActivity.java
@@ -1,19 +1,41 @@
 package com.wsoteam.diet.presentation.promo;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.arellomobile.mvp.MvpAppCompatActivity;
+import com.google.android.material.textfield.TextInputLayout;
 import com.wsoteam.diet.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class PromoFormActivity extends MvpAppCompatActivity implements PromoFormView {
+    @BindView(R.id.btnSendPromo) Button btnSendPromo;
+    @BindView(R.id.edtPromo) EditText edtPromo;
+    @BindView(R.id.tilPromo) TextInputLayout tilPromo;
     private PromoFormPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_promo_form);
+        ButterKnife.bind(this);
 
         presenter = new PromoFormPresenter(this);
         presenter.attachView(this);
+    }
+
+    @OnClick({R.id.ibBack, R.id.btnSendPromo})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.ibBack:
+                break;
+            case R.id.btnSendPromo:
+                break;
+        }
     }
 }
