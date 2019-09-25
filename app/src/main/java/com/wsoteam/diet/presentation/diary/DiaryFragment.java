191e4cd29161f2441d54a8d930db8818930caffa
ozz <ozik.dev@gmail.com>
2019-09-25 12:52:25 +0500

    - fixed gradaient - added new diary fragment - added widget lifecycle
    TN-9688
    TN-10732
    TN-9565

---
+++ app/src/main/java/com/wsoteam/diet/presentation/diary/DiaryFragment.java
@@ -0,0 +1,28 @@
+package com.wsoteam.diet.presentation.diary;
+
+import android.view.View;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class DiaryFragment extends Fragment {
+
+  class WidgetView extends RecyclerView.ViewHolder {
+
+    public WidgetView(@NonNull View itemView) {
+      super(itemView);
+    }
+
+    protected void onAttached(RecyclerView parent) {
+
+    }
+
+    protected void onDetached(RecyclerView parent) {
+
+    }
+
+    protected void onBind(RecyclerView.Adapter<WidgetView> dataAdapter) {
+
+    }
+  }
+}
