f991a789d34a8a81517f3fe8ed71ba95653a7f57
Wadim <waadimka@mail.ru>
2019-09-30 17:23:34 +0300

    sort
    TN-10605
    /
    
    TN-11165
    Скопировать ссылку на эту задачу

--- app/src/main/java/com/wsoteam/diet/presentation/measurment/history/fragment/MeasHistoryPresenter.java
+++ app/src/main/java/com/wsoteam/diet/presentation/measurment/history/fragment/MeasHistoryPresenter.java
@@ -2,6 +2,14 @@ package com.wsoteam.diet.presentation.measurment.history.fragment;
 
 import com.arellomobile.mvp.InjectViewState;
 import com.arellomobile.mvp.MvpPresenter;
+import com.wsoteam.diet.Sync.UserDataHolder;
+import com.wsoteam.diet.presentation.measurment.POJO.Weight;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 @InjectViewState
 public class MeasHistoryPresenter extends MvpPresenter<MeasHistoryView> {
@@ -9,6 +17,8 @@ public class MeasHistoryPresenter extends MvpPresenter<MeasHistoryView> {
     private final int MEAS_WAIST = 1;
     private final int MEAS_CHEST = 2;
     private final int MEAS_HIPS = 3;
+    private ArrayList<String> keys = new ArrayList<>();
+    private ArrayList<String> values = new ArrayList<>();
 
     public MeasHistoryPresenter() {
     }
@@ -40,5 +50,24 @@ public class MeasHistoryPresenter extends MvpPresenter<MeasHistoryView> {
     }
 
     private void getWeightHistory() {
+        if (UserDataHolder.getUserData().getWeights() != null && UserDataHolder.getUserData().getWeights().size() > 0){
+            HashMap<String, Weight> weightHashMap = UserDataHolder.getUserData().getWeights();
+            keys = new ArrayList<>();
+            Iterator iterator = weightHashMap.entrySet().iterator();
+            while (iterator.hasNext()){
+                Map.Entry pair = (Map.Entry) iterator.next();
+                keys.add((String) pair.getKey());
+            }
+            Collections.sort(keys);
+
+            values = new ArrayList<>();
+            for (int i = 0; i < keys.size(); i++) {
+                values.add(String.valueOf(weightHashMap.get(keys.get(i)).getWeight()));
+            }
+        }else {
+            keys = new ArrayList<>();
+            values = new ArrayList<>();
+        }
+
     }
 }
