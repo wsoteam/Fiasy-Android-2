4debfecb0616add9a13c51124365a86690470580
Wadim <waadimka@mail.ru>
2019-10-11 20:57:24 +0300

    help
    TN-11751
    /
    
    TN-12111
    Скопировать ссылку на эту задачу
    TN-11751
    /
    
    TN-12112
    Скопировать ссылку на эту задачу

--- app/src/main/java/com/wsoteam/diet/presentation/measurment/help/HelpActivity.java
+++ app/src/main/java/com/wsoteam/diet/presentation/measurment/help/HelpActivity.java
@@ -3,11 +3,18 @@ package com.wsoteam.diet.presentation.measurment.help;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import com.wsoteam.diet.R;
 
 public class HelpActivity extends AppCompatActivity {
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_meas_help);
+    ButterKnife.bind(this);
+  }
+
+  @OnClick(R.id.ibBack) public void onViewClicked() {
+    onBackPressed();
   }
 }
