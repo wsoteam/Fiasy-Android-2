c4ccfaa9272f0d0ce51ba89ab56fb4c212cdf4b6
Wadim <waadimka@mail.ru>
2019-10-13 19:10:00 +0300

    search_item
    https://wsoteam.atlassian.net/browse/TN-12152

---
+++ app/src/main/java/com/wsoteam/diet/presentation/search/results/controller/ResultViewHolder.java
@@ -0,0 +1,14 @@
+package com.wsoteam.diet.presentation.search.results.controller;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.wsoteam.diet.R;
+
+public class ResultViewHolder extends RecyclerView.ViewHolder {
+  public ResultViewHolder(@NonNull LayoutInflater layoutInflater, ViewGroup viewGroup) {
+    super(layoutInflater.inflate(R.layout.item_search_result));
+  }
+}
