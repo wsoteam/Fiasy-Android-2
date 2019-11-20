9bfaa862dd6915c9cb2eac1676ab1e972858b54e
Nikolay <ny@wsoteam.com>
2019-11-20 20:21:53 +0300

    - fixed background (recipe, articles, plans)
    TN-14933
    TN-15121

---
+++ app/src/main/java/com/wsoteam/diet/utils/Img.java
@@ -0,0 +1,26 @@
+package com.wsoteam.diet.utils;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.widget.LinearLayout;
+
+import androidx.core.graphics.ColorUtils;
+import androidx.palette.graphics.Palette;
+
+public class Img {
+    public static void setBackGround(Drawable drawable, LinearLayout layout){
+        Bitmap bitmap;
+
+        if (drawable instanceof BitmapDrawable) {
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
+            bitmap = bitmapDrawable.getBitmap();
+            if(bitmap != null) {
+                Palette p = Palette.from(bitmap).generate();
+                int mainColor = p.getMutedColor(0);
+                int alphaColor = 191;
+                layout.setBackgroundColor(ColorUtils.setAlphaComponent(mainColor, alphaColor));
+            }
+        }
+    }
+}
