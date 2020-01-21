49ceb9b98f88dbd1ac1212f1a0bccd6b571e92a3
Nikolay <ny@wsoteam.com>
2020-01-21 12:59:19 +0300

    - removed trash
    TN-18039

--- app/src/main/java/com/wsoteam/diet/presentation/training/TrainingViewModel.kt
+++ app/src/main/java/com/wsoteam/diet/presentation/training/TrainingViewModel.kt
@@ -30,34 +30,5 @@ class TrainingViewModel : ViewModel() {
     }
 
     fun getTrainings(): LiveData<MapTraining> = trainings
-
-
-    private fun loadTrainings(liveData: MutableLiveData<MapTraining>) {
-        val mapTraining = MapTraining()
-
-        val training = Training()
-        training.url = "https://firebasestorage.googleapis.com/v0/b/diet-for-test.appspot.com/o/PlansRecipes%2F01new%2F228.jpg?alt=media&token=561223b8-f936-494b-a4f5-cff4f6bd10ff"
-        training.name = "test"
-
-        for (i in 1..28) {
-            (training.days as MutableMap)["day-$i"] = TrainingDay(exercises = getDays(), day = i)
-        }
-
-        training.uid = "buttock_leg_training"
-        (mapTraining.trainings as MutableMap)["buttock_leg_training"] = training
-        training.uid = "full_body_workout"
-        (mapTraining.trainings as MutableMap)["full_body_workout"] = training
-
-        liveData.value = mapTraining
-    }
-
-    private fun getDays(): MutableMap<String, Exercises> {
-
-        val days = mutableMapOf<String, Exercises>()
-        for (i in 1..10) {
-            days["exercises-$i"] = Exercises(type = "bent_knee_raise", number = i)
-        }
-
-        return days
-    }
+    
 }
