c910806caf4d10fbe88100fbfcabc87e3bfa4698
Nikolay <ny@wsoteam.com>
2020-01-24 17:16:42 +0300

    - fix days picker
    TN-6560
    TN-18711

--- app/src/main/java/com/wsoteam/diet/presentation/starvation/DaysPickerDialogFragment.kt
+++ app/src/main/java/com/wsoteam/diet/presentation/starvation/DaysPickerDialogFragment.kt
@@ -5,10 +5,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentManager
-import androidx.lifecycle.Observer
 import com.wsoteam.diet.R
 import com.wsoteam.diet.Sync.WorkWithFirebaseDB
 import kotlinx.android.synthetic.main.fragment_days_picker_dialog.*
@@ -17,11 +17,11 @@ import java.util.*
 
 class DaysPickerDialogFragment : DialogFragment() {
 
-    companion object{
+    companion object {
         fun newInstance() = DaysPickerDialogFragment()
 
         fun show(fragmentManager: FragmentManager?): DaysPickerDialogFragment? {
-            if (fragmentManager == null ) return null
+            if (fragmentManager == null) return null
 
             val dialog = newInstance()
 
@@ -33,71 +33,52 @@ class DaysPickerDialogFragment : DialogFragment() {
     private var _style = STYLE_NO_TITLE
     private var _theme = R.style.FullScreenDialog_NoStatusBar
 
-    private val starvationDays = mutableSetOf<Int>()
+    private lateinit var checkBoxMap: Map<Int, CheckBox>
+    private val starvationDays: MutableSet<Int> = StarvationViewModel.getStarvation().value?.days?.toMutableSet()
+            ?: mutableSetOf()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setStyle(_style, _theme)
-        StarvationViewModel.getStarvation().observe(this, Observer {
-            starvationDays.clear()
-            it.days?.forEach { d -> starvationDays.add(d) }
-        })
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_days_picker_dialog, container, false)
         view.findViewById<CardView>(R.id.daysCard).setBackgroundResource(R.drawable.teach_cardview_back)
-
         return view
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
         cancel.setOnClickListener { dismiss() }
 
         ok.setOnClickListener {
-
-            WorkWithFirebaseDB.setStarvationDays(mutableListOf(1, 5, 6, 7))
+            WorkWithFirebaseDB.setStarvationDays(starvationDays.toList())
+            dismiss()
         }
 
-        checkBoxMonday.setOnCheckedChangeListener { buttonView, isChecked ->
+        checkBoxMap = mutableMapOf(
+                Calendar.MONDAY to checkBoxMonday,
+                Calendar.TUESDAY to checkBoxTuesday,
+                Calendar.WEDNESDAY to checkBoxWednesday,
+                Calendar.THURSDAY to checkBoxThursday,
+                Calendar.FRIDAY to checkBoxFriday,
+                Calendar.SATURDAY to checkBoxSaturday,
+                Calendar.SUNDAY to checkBoxSunday
+        )
 
-        }
-        checkBoxTuesday.setOnCheckedChangeListener { buttonView, isChecked ->
-
-        }
-        checkBoxWednesday.setOnCheckedChangeListener { buttonView, isChecked ->
-
-        }
-
-        checkBoxThursday.setOnCheckedChangeListener { buttonView, isChecked ->
-
-        }
-
-        checkBoxFriday.setOnCheckedChangeListener { buttonView, isChecked ->
-
-        }
-
-        checkBoxSaturday.setOnCheckedChangeListener { buttonView, isChecked ->
-
-        }
-
-        checkBoxSunday.setOnCheckedChangeListener { buttonView, isChecked ->
-
-        }
-
-    }
-
-    private fun upadteUi(){
-        starvationDays.forEach { d ->
-            when(d){
-                Calendar.MONDAY -> checkBoxMonday.isEnabled = true
-                Calendar.TUESDAY -> checkBoxTuesday.isEnabled = true
+        for (i in 1..7) {
+            checkBoxMap[i]?.setOnCheckedChangeListener { buttonView, isChecked ->
+                if (isChecked) starvationDays.add(i)
+                else starvationDays.remove(i)
             }
         }
+
+        starvationDays.forEach { checkBoxMap[it]?.isChecked = true }
     }
+
 }
