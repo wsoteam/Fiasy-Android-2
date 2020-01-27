904e9773a4ae3f126a41ea89513fd19dd70c594f
Wadim <waadimka@mail.ru>
2020-01-27 15:27:07 +0300

    ui
    https://wsoteam.atlassian.net/browse/TN-18777

---
+++ app/src/main/java/com/wsoteam/diet/presentation/premium/GraphPrePremium.kt
@@ -0,0 +1,8 @@
+package com.wsoteam.diet.presentation.premium
+
+import androidx.appcompat.app.AppCompatActivity
+import com.wsoteam.diet.R
+
+class GraphPrePremium : AppCompatActivity(R.layout.activity_pre_prem_graph) {
+
+}
