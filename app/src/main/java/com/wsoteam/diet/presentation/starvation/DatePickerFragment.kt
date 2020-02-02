556e78f8c03089b3c2094ef913c167d05382da3d
Nikolay <ny@wsoteam.com>
2020-02-02 17:21:19 +0300

    - date picker
    TN-18818
    TN-19010

---
+++ app/src/main/java/com/wsoteam/diet/presentation/starvation/DatePickerFragment.kt
@@ -0,0 +1,75 @@
+package com.wsoteam.diet.presentation.starvation
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.DatePicker
+import androidx.fragment.app.DialogFragment
+import com.wsoteam.diet.R
+import java.util.*
+
+class DatePickerFragment: DialogFragment() {
+
+    companion object{
+        private const val ARG_DATE = "date"
+        const val EXTRA_DATE = "DatePickerFragment.date"
+
+        fun newInstance(calendar: Calendar): DatePickerFragment{
+            val args = Bundle()
+            args.putLong(ARG_DATE, calendar.timeInMillis)
+
+            val datePickerFragment = DatePickerFragment()
+            datePickerFragment.arguments = args
+            return datePickerFragment
+        }
+    }
+
+
+    private lateinit var datePicker: DatePicker
+
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        val date = Calendar.getInstance()
+        val prevDate = Calendar.getInstance()
+
+        arguments?.getLong(ARG_DATE)?.apply { date.timeInMillis = this }
+        prevDate.time = date.time
+        prevDate.add(Calendar.DATE, -3)
+
+        val view = LayoutInflater.from(context).inflate(R.layout.starvation_date_picker, null)
+
+        datePicker = view.findViewById(R.id.starvation_date_picker)
+        datePicker.minDate = prevDate.timeInMillis
+        datePicker.init(date.get(Calendar.YEAR), date.get(Calendar.MONTH), date.get(Calendar.DAY_OF_MONTH), null)
+
+        val dialog = AlertDialog.Builder(context)
+                .setView(view)
+                .setPositiveButton(android.R.string.ok) { dialog, which ->
+                    val calendar = GregorianCalendar(datePicker.year, datePicker.month, datePicker.dayOfMonth)
+                    sendResult(Activity.RESULT_OK, calendar.timeInMillis)
+                }
+                .setNegativeButton(R.string.starvation_close, null)
+                .create()
+
+        dialog.setCanceledOnTouchOutside(false)
+
+        val negativeButton = dialog.getButton(DialogInterface.BUTTON_NEGATIVE)
+        negativeButton?.setTextColor(Color.parseColor("#717171"))
+        negativeButton?.isAllCaps = false
+        return dialog
+    }
+
+    private fun sendResult(resultCode: Int, date: Long){
+
+        val intent = Intent()
+        intent.putExtra(EXTRA_DATE, date)
+        targetFragment?.onActivityResult(targetRequestCode, resultCode, intent)
+    }
+}
--- app/src/main/java/com/wsoteam/diet/presentation/starvation/StarvationSettingsFragment.kt
+++ app/src/main/java/com/wsoteam/diet/presentation/starvation/StarvationSettingsFragment.kt
@@ -1,12 +1,14 @@
 package com.wsoteam.diet.presentation.starvation
 
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
 import com.wsoteam.diet.R
 import kotlinx.android.synthetic.main.fragment_starvation_settings.*
 import android.app.TimePickerDialog
+import android.content.Intent
 import android.graphics.Color
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
@@ -19,6 +21,9 @@ import androidx.core.graphics.drawable.DrawableCompat
 
 class StarvationSettingsFragment : Fragment(R.layout.fragment_starvation_settings) {
 
+    private val startDate = Calendar.getInstance()
+
+    private val REQUEST_DATE = 0
     private lateinit var daysWeek: List<String>
     private var isStart = false
 
@@ -38,9 +43,17 @@ class StarvationSettingsFragment : Fragment(R.layout.fragment_starvation_setting
             activity?.onBackPressed()
         }
 
-        days.setOnClickListener {
-            DaysPickerDialogFragment.show(fragmentManager)
+        date.setOnClickListener {
+            val dialog = DatePickerFragment.newInstance(Calendar.getInstance())
+            val fm = activity?.supportFragmentManager
+            dialog.setTargetFragment(this, REQUEST_DATE)
+            fm?.let { dialog.show(fm, dialog.javaClass.name) }
+
+//            val dialog = DatePickerFragment.newInstance(Calendar.getInstance())
+//            dialog.setTargetFragment(this, REQUEST_DATE)
+//            dialog.show(fragmentManager!!, DatePickerFragment::class.java.simpleName)
         }
+
         time.setOnClickListener { openDialog() }
 
         toolbar.setNavigationOnClickListener { activity?.onBackPressed() }
@@ -66,6 +79,8 @@ class StarvationSettingsFragment : Fragment(R.layout.fragment_starvation_setting
 
     private fun updateUi(starvation: Starvation) {
 
+
+
         val dayResult = StringBuffer()
         val daysList = starvation.days.toMutableList()
 
@@ -79,7 +94,8 @@ class StarvationSettingsFragment : Fragment(R.layout.fragment_starvation_setting
         }
 
 
-        when {
+
+/*        when {
             daysList.size == 0 -> days.text = getString(R.string.starvation_select)
             daysList.size == Calendar.DAY_OF_WEEK -> days.text = getString(R.string.starvation_all_days)
             else -> {
@@ -97,7 +113,7 @@ class StarvationSettingsFragment : Fragment(R.layout.fragment_starvation_setting
                 dayResult.delete(0, 2)
                 days.text = dayResult
             }
-        }
+        }*/
 
         val buttonDrawable = DrawableCompat.wrap(start.background)
 
@@ -112,11 +128,33 @@ class StarvationSettingsFragment : Fragment(R.layout.fragment_starvation_setting
         start.background = buttonDrawable
     }
 
+    private fun updateDate(){
+        val dateFormat = SimpleDateFormat("E, d MMM", Locale.getDefault())
+        date.text = dateFormat.format(startDate.time)
+    }
+
     override fun onPause() {
         if (!isStart) {
             (StarvationViewModel.getStarvation() as MutableLiveData).value = Starvation()
             WorkWithFirebaseDB.deleteStarvation()
         }
         super.onPause()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode != Activity.RESULT_OK) return
+
+       when (requestCode){
+           REQUEST_DATE -> {
+               data?.getLongExtra(DatePickerFragment.EXTRA_DATE, Calendar.getInstance().timeInMillis)?.apply {
+                   val calendar = Calendar.getInstance()
+                   calendar.timeInMillis = this
+                   startDate.set(Calendar.YEAR, calendar.get(Calendar.YEAR))
+                   startDate.set(Calendar.MONTH, calendar.get(Calendar.MONTH))
+                   startDate.set(Calendar.DAY_OF_MONTH, calendar.get(Calendar.DAY_OF_MONTH))
+                   updateDate()
+               }
+           }
+       }
     }
 }
