5cf7ce91a679d10bae4817616aa6ce7d5270f327
Nikolay <ny@wsoteam.com>
2020-02-06 10:19:24 +0300

    - save notification setting
    TN-19081
    TN-19204

--- app/src/main/java/com/wsoteam/diet/presentation/starvation/Utility.kt
+++ app/src/main/java/com/wsoteam/diet/presentation/starvation/Utility.kt
@@ -5,14 +5,59 @@ import android.content.Context
 class Utility {
     companion object{
 
-        const val STARVATION_TIME_MILLIS = "com.wsoteam.diet.presentation.starvation.time"
+        const val STARVATION_PREFERENCES = "com.wsoteam.diet.presentation.starvation"
+        const val TIMESTAMP = "PREF_TIMESTAMP"
+        const val BASIC_NOTIFICATION = "BASIC_NOTIFICATION"
+        const val ADVANCE_NOTIFICATION = "ADVANCE_NOTIFICATION"
 
-        fun saveStarvationTime(context: Context?){
-            val sharedPref = context?.getSharedPreferences(STARVATION_TIME_MILLIS, Context.MODE_PRIVATE) ?: return
-            with (sharedPref.edit()) {
-//                putInt(getString(R.string.saved_high_score_key), newHighScore)
-//                commit()
+        fun setLocalStarvationTime(context: Context?, timestamp: Long) {
+            context?.apply {
+                val sharedPref = context.getSharedPreferences(STARVATION_PREFERENCES, Context.MODE_PRIVATE)
+                        ?: return
+                with(sharedPref.edit()) {
+                    putLong(TIMESTAMP, timestamp)
+                    apply()
+                }
             }
+        }
+
+        fun getLocalStarvationTime(context: Context?): Long{
+            context?.apply {
+                val sharedPref = context.getSharedPreferences(STARVATION_PREFERENCES, Context.MODE_PRIVATE)
+                        ?: return 0
+                return sharedPref.getLong(TIMESTAMP, 0)
+            }
+            return 0
+        }
+
+        fun setNotificationSetting(context: Context?, basicNotification: Boolean,  advanceNotification: Boolean){
+            context?.apply {
+                val sharedPref = context.getSharedPreferences(STARVATION_PREFERENCES, Context.MODE_PRIVATE)
+                        ?: return
+                with(sharedPref.edit()) {
+                    putBoolean(BASIC_NOTIFICATION, basicNotification)
+                    putBoolean(ADVANCE_NOTIFICATION, advanceNotification)
+                    apply()
+                }
+            }
+        }
+
+        fun isNeedBasicNotification(context: Context?): Boolean{
+            context?.apply {
+                val sharedPref = context.getSharedPreferences(STARVATION_PREFERENCES, Context.MODE_PRIVATE)
+                        ?: return false
+                return sharedPref.getBoolean(BASIC_NOTIFICATION, false)
+            }
+            return false
+        }
+
+        fun isNeedAdvanceNotification(context: Context?): Boolean{
+            context?.apply {
+                val sharedPref = context.getSharedPreferences(STARVATION_PREFERENCES, Context.MODE_PRIVATE)
+                        ?: return false
+                return sharedPref.getBoolean(ADVANCE_NOTIFICATION, false)
+            }
+            return false
         }
     }
 }
