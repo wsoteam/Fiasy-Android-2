bc698986c4bc8782e4d2352e399b5c76f32822ee
Nikolay <ny@wsoteam.com>
2020-02-11 22:35:54 +0300

    - notification setting checking
    TN-19345
    TN-19418

--- app/src/main/java/com/wsoteam/diet/presentation/starvation/StarvationNotificationFragment.kt
+++ app/src/main/java/com/wsoteam/diet/presentation/starvation/StarvationNotificationFragment.kt
@@ -2,7 +2,9 @@ package com.wsoteam.diet.presentation.starvation
 
 
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
+import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
@@ -18,12 +20,14 @@ import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
 import android.os.Build
 import android.provider.Settings.EXTRA_APP_PACKAGE
 import android.provider.Settings.ACTION_APP_NOTIFICATION_SETTINGS
-
-
-
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.dialog_fragment_abort_exercise.*
 
 
 class StarvationNotificationFragment : Fragment() {
+
+    private var nDialog : AlertDialog? = null
+    private var isSettingOpen = false
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -32,8 +36,6 @@ class StarvationNotificationFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        checkNotification(context)
 
         toolbar3.setNavigationOnClickListener { fragmentManager?.popBackStack() }
 
@@ -59,7 +61,8 @@ class StarvationNotificationFragment : Fragment() {
         context?.apply {
             val mNotificationManagerCompat: NotificationManagerCompat = NotificationManagerCompat.from(context)
             val areNotificationsEnabled = mNotificationManagerCompat.areNotificationsEnabled()
-            if (!areNotificationsEnabled) openNotificationSettingsForApp(context)
+            Log.d("kkk", "check = ${!areNotificationsEnabled}")
+            if (!areNotificationsEnabled) notificationDialog(context)
         }
     }
 
@@ -82,5 +85,53 @@ class StarvationNotificationFragment : Fragment() {
             }
         }
         context.startActivity(intent)
+    }
+
+    private fun notificationDialog(context: Context): AlertDialog? {
+
+        if (nDialog == null) {
+            Log.d("kkk", "nDialog == null")
+            val dialog = AlertDialog.Builder(context)
+                    .setTitle(R.string.starvation_notification_alert_title)
+                    .setMessage(R.string.starvation_notification_alert_txt)
+                    .setPositiveButton(R.string.starvation_setting) { _, _ ->
+                        nDialog = null
+                        isSettingOpen = true
+                        openNotificationSettingsForApp(context)
+                    }
+                    .setNegativeButton(R.string.starvation_cancle) { _, _ ->
+                        nDialog = null
+                        fragmentManager?.popBackStack()
+                    }
+                    .create()
+
+            dialog.setCanceledOnTouchOutside(false)
+
+            dialog.show()
+
+
+            val positiveButton = dialog.getButton(DialogInterface.BUTTON_POSITIVE)
+            positiveButton.setTextColor(Color.parseColor("#8a000000"))
+
+            dialog.setOnCancelListener {
+                Log.d("kkk", "setOnCancelListener")
+                nDialog = null
+
+                fragmentManager?.popBackStack()
+
+            }
+
+            nDialog = dialog
+            return dialog
+        } else {
+            Log.d("kkk", "nDialog != null")
+            return null
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("kkk", "res++++")
+        checkNotification(context)
     }
 }
